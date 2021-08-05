2331dc4105c219c6b0acf20eb3a2a988b33621e0
Davi de Souza Silva <davi.silva41@fatec.sp.gov.br>
2021-08-05 19:08:25 -0300

    finalizing course

--- projeto-glass-html5/_javascript/funcoes.js
+++ projeto-glass-html5/_javascript/funcoes.js
@@ -1,3 +1,18 @@
 function mudaFoto (foto){
     document.getElementById("icone").src= foto;
 }
+/*function id(valor_campo)
+{
+    return document.getElementById(valor_campo)
+}
+function getValor()
+{
+    var valor = document.getElementById(valorCampo).value.replace(',','.');
+    document.write(valor)
+    return parseFloat(valor) * 100
+}
+function multi()
+{
+ var total = getValor('cQtd')+ getValor('cUnit');
+ id('cPreco').value = total/100;
+}*/
